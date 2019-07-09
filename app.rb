622b21acee82cb236ea9f4460b288bb2ffe9af6e
gillianwenhold <gwenhold@mac.com>
2019-07-09 11:10:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Gillian."
+  end
+
+  get "/hometown" do
+    "My hometown is Collegeville"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Walking in Memphis"
+  end
 end
